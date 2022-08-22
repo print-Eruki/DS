809d1e53d8dfe7fe82409efe9e4d2d87a7dce4ff
Ale <palejorniel@gmail.com>
2022-08-22 16:08:01 -0400

    part 2 of lab1

---
+++ lab1/src/main/java/part2.java
@@ -0,0 +1,48 @@
+public class Lab0 {
+
+	public static class Sports {
+		private String name;
+		private int players;
+		public String testValue; //DO NOT DELETE THIS
+		
+		public Sports(String name, int players) {
+			this.name = name;
+			this.players = players;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public int getPlayers() {
+			return players;
+		}
+		
+		public String printTeamMembers() {
+			return "Each team has " + getPlayers() + " players in " + getName();
+		}
+		
+		//DO NOT DELETE THIS
+		public void setTestValue() {
+			testValue = printTeamMembers();
+		}
+	}
+
+	public static class Soccer extends Sports {
+		
+		public Soccer(String name, int players) {
+			super(name, players);
+		}
+
+		/*TODO ADD OVERWRITTEN METHODS HERE*/
+		@Override
+		public String getName() { return "Overwritten Soccer";}
+		
+		@Override
+		public int getPlayers() { return 11;}
+		
+		@Override
+		public String printTeamMembers() {
+			return "Each team has " + getPlayers() + " players in " + getName();
+		}
+}
